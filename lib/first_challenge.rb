e7c8816cbdfa7623bdd9a30e90bbc24c47d4d490
Elaina <elainaseki@gmail.com>
2019-02-19 03:57:29 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -16,7 +16,7 @@ def first_challenge
   #your code here
 contacts.each do|person, data|
   data.each do|attribute, value|
-   if attribute == contacts["Freddy Mercury"][:favorite_icecream_flavors]
+   if attribute ==  ["strawberry", "cookie dough", "mint chip"]
    attribute.delete_if |v|
      v == "strawberry"
    end
