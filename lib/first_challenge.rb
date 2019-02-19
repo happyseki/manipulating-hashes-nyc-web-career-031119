9da684a396345a2410aed7575e9984be0f95f425
Elaina <elainaseki@gmail.com>
2019-02-19 03:50:32 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -17,7 +17,7 @@ def first_challenge
 contacts.each do|person, data|
   data.each do|attribute, value|
    if attribute == :favorite_icecream_flavors
-    attribute = ["strawberry", "cookie dough", "mint chip"]
+    attribute = ["chocolate", "cookie dough", "mint chip"]
     end
    end
   end
